4172f1b62ce743c9143a7ddcf037eb3fc71ecb66
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-12-02 19:17:38 -0300

    Create UserDbContext

---
+++ JWTAuthentication/Data/UserDbContext.cs
@@ -0,0 +1,10 @@
+﻿using JWTAuthentication.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTAuthentication.Data
+{
+    public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+    {
+        DbSet<User> Users { get; set; }
+    }
+}
