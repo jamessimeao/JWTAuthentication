7a587d8ca37ad6c4ba97b0b5a0fc8c02f3e5914a
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-12-02 19:51:21 -0300

    Make UserDbContext properties public

--- JWTAuthentication/Data/UserDbContext.cs
+++ JWTAuthentication/Data/UserDbContext.cs
@@ -5,6 +5,6 @@ namespace JWTAuthentication.Data
 {
     public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
     {
-        DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
