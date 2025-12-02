8a65a1aab5f77196a6d734e9cb90a99797af20a7
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-12-02 19:53:19 -0300

    Replace AuthController code with AuthService calls

--- JWTAuthentication/Controllers/AuthController.cs
+++ JWTAuthentication/Controllers/AuthController.cs
@@ -1,75 +1,40 @@
 ﻿using JWTAuthentication.Dtos;
 using JWTAuthentication.Entities;
-using Microsoft.AspNetCore.Identity;
+using JWTAuthentication.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
-using System.Text;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace JWTAuthentication.Controllers
 {
     [ApiController]
     [Route("api/[controller]/[action]")]
-    public class AuthController(IConfiguration configuration) : ControllerBase
+    public class AuthController(IAuthService authService) : ControllerBase
     {
-        // Temporary, until there is no database
-        private static User user = new User();
-
         [HttpPost]
-        public ActionResult<User> Register(UserDto userDto)
+        public async Task<ActionResult<User>> RegisterAsync(UserDto userDto)
         {
-            string passwordHash = new PasswordHasher<User>().HashPassword(user, userDto.Password);
-            user.Username = userDto.Username;
-            user.PasswordHash = passwordHash;
-            return Ok(user);
+            User? user = await authService.RegisterAsync(userDto);
+            if(user != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return BadRequest("Username already exists.");
+            }
         }
 
         [HttpPost]
-        public ActionResult<string> Login(UserDto userDto)
+        public async Task<ActionResult<string>> LoginAsync(UserDto userDto)
         {
-            bool authenticated = false;
-            if(userDto.Username == user.Username)
+            string? token = await authService.LoginAsync(userDto);
+            if(token != null)
             {
-                PasswordVerificationResult result = new PasswordHasher<User>().
-                                                        VerifyHashedPassword(user, user.PasswordHash, userDto.Password);
-                
-                 if(result == PasswordVerificationResult.Success)
-                {
-                    authenticated = true;
-                }
-            }
-
-            if (authenticated)
-            {
-                string token = CreateToken(user);
                 return Ok(token);
             }
             else
             {
                 return BadRequest("Wrong username or password.");
             }
-        }
-
-        private string CreateToken(User user)
-        {
-            Dictionary<string, object> claims = new Dictionary<string, object>()
-            {
-                [ClaimTypes.Name] = user.Username
-            };
-            string config = configuration.GetValue<string>("AppSettings:Token")!;
-            byte[] data = Encoding.UTF8.GetBytes(config);
-            SymmetricSecurityKey key = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(data);
-            SigningCredentials credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha512);
-            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor()
-            {
-                Issuer = configuration.GetValue<string>("AppSettings:Issuer")!,
-                Audience = configuration.GetValue<string>("AppSettings:Audience")!,
-                Claims = claims,
-                Expires = DateTime.UtcNow.AddDays(1),
-                SigningCredentials = credentials
-            };
-            return new JsonWebTokenHandler().CreateToken(tokenDescriptor);
         }
     }
 }
