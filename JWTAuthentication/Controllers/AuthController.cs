ad9c3b3fa308c2433f1210d195278a0824ba0e4c
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-12-02 15:50:40 -0300

    Create AuthController with Register action
    The Register action registers a new user. It receives a UserDto, with username and password, and returns the User with a hashed password.

---
+++ JWTAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using JWTAuthentication.Dtos;
+using JWTAuthentication.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthentication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto userDto)
+        {
+            string hashedPassword = new PasswordHasher<User>().HashPassword(user, userDto.Password);
+            user.Username = userDto.Username;
+            user.PasswordHash = hashedPassword;
+            return Ok(user);
+        }
+
+    }
+}
