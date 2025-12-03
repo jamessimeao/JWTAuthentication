8ba479967c47637dba89930c2bbdc595554b3152
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-12-03 14:47:38 -0300

    Create TokenResponseDto
    Dto with access token and refresh token

---
+++ JWTAuthentication/Dtos/TokenResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTAuthentication.Dtos
+{
+    public class TokenResponseDto
+    {
+        public required string AccessToken { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
