500efda6f829035149f662d3946ad6325697a481
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-12-03 11:40:32 -0300

    Add RefreshToken to User model

--- JWTAuthentication/Entities/User.cs
+++ JWTAuthentication/Entities/User.cs
@@ -9,5 +9,7 @@ namespace JWTAuthentication.Entities
         public string Username { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
     }
 }
