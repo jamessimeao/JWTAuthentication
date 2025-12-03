4f9693421d65635273addc947f44938cd142b6ae
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-12-02 21:36:45 -0300

    Add a Role property to the User model

--- JWTAuthentication/Entities/User.cs
+++ JWTAuthentication/Entities/User.cs
@@ -8,5 +8,7 @@ namespace JWTAuthentication.Entities
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
     }
 }
