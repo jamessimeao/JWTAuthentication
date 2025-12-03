06cda349ef2092a37170b2aa96ebc5f5f93d42ad
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-12-02 21:42:45 -0300

    Add migration to make Role column in Users table

--- JWTAuthentication/Data/Migrations/20251203004037_AddRoleToUser.Designer.cs
+++ JWTAuthentication/Data/Migrations/20251203004037_AddRoleToUser.Designer.cs
@@ -3,6 +3,7 @@ using JWTAuthentication.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JWTAuthentication.Data.Migrations
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251203004037_AddRoleToUser")]
+    partial class AddRoleToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
