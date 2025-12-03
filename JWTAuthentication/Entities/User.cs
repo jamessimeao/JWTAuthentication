daaf58acca2e30e76173087624a1f734f712ff37
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-12-02 21:39:27 -0300

    Remove blank line

--- JWTAuthentication/Entities/User.cs
+++ JWTAuthentication/Entities/User.cs
@@ -8,7 +8,6 @@ namespace JWTAuthentication.Entities
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
-
         public string Role { get; set; } = string.Empty;
     }
 }
